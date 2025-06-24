2c07f9bf3234b0a8cbd9839ca37dea1baf23278e
AbrAlexis <alexalex2620@gmail.com>
2025-06-24 23:59:09 +0200

    Added a function that makes a solution gird and a working grid

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"regexp"
 )
 
+func copyStringSliceGrid(grid [][]string) [][]string {
+	copyGrid := make([][]string, len(grid))
+	for i := range grid {
+		copyGrid[i] = make([]string, len(grid[i]))
+		copy(copyGrid[i], grid[i])
+	}
+	return copyGrid
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
-}
-
-func getDimensions(puzzle string) (rows int, cols int) {
-	lines := regexp.MustCompile("\n").Split(puzzle, -1)
-	return len(lines), len(lines[0])
-	// return lines.len(), lines[0].len
 }
 
 func getCompletePuzzle(xdPuzzlePath string) (string, error) {
@@ -27,6 +30,12 @@ func getCompletePuzzle(xdPuzzlePath string) (string, error) {
 	}
 }
 
+func getDimensions(puzzle string) (rows int, cols int) {
+	lines := regexp.MustCompile("\n").Split(puzzle, -1)
+	return len(lines), len(lines[0])
+	// return lines.len(), lines[0].len
+}
+
 func getPuzzle(completePuzzle string) string {
 	re := regexp.MustCompile(`(?s)\n\n\n(.*?)\n\n`)
 	found := re.FindStringSubmatch(completePuzzle)
@@ -34,7 +43,7 @@ func getPuzzle(completePuzzle string) string {
 	return grid
 }
 
-func createAnswerGrid(completePuzzle string) [][]string {
+func createCompletedPuzzle(completePuzzle string) [][]string {
 	nRows, nCols := getDimensions(completePuzzle)
 	//Create the empty puzzle grid, later to be filled out
 	answerGrid := make([][]string, nRows)
@@ -46,24 +55,46 @@ func createAnswerGrid(completePuzzle string) [][]string {
 	completePuzzleAsRune := []rune(completePuzzle)
 	counter := 0
 	for i := range answerGrid {
-		for j := range answerGrid[i] {
+		j := 0
+		for j < nCols {
 			if completePuzzleAsRune[counter] != '\n' {
 				answerGrid[i][j] = string(completePuzzleAsRune[counter])
-				counter++
-			} else {
-				counter++
-				continue
-
+				j++
 			}
-
+			counter++
 		}
 	}
 	return answerGrid
 }
 
+func createPuzzleToBeFilled(answerGrid [][]string) [][]string {
+	workingPuzzle := copyStringSliceGrid(answerGrid)
+	for i := range workingPuzzle {
+		for j := range workingPuzzle[i] {
+			if workingPuzzle[i][j] != "#" {
+				workingPuzzle[i][j] = ""
+			}
+		}
+	}
+	return workingPuzzle
+}
+
+func initializeGame(filePath string) (workingPuzzle [][]string, completedPuzzle [][]string) {
+	puzzleAllData, _ := getCompletePuzzle(filePath)
+	onlyGrid := getPuzzle(puzzleAllData)
+	completedPuzzleReturn := createCompletedPuzzle(onlyGrid)
+	workingPuzzleReturn := createPuzzleToBeFilled(completedPuzzleReturn)
+	return workingPuzzleReturn, completedPuzzleReturn
+}
+
 func main() {
-	puzzle, _ := getCompletePuzzle("/home/abralexis/lavkrydsord/xdData/gxd/aarp/1998/mm1998-05-06.xd")
-	onlyPuzzle := getPuzzle(puzzle)
-	//fmt.Println(getDimensions(onlyPuzzle))
-	fmt.Println(createAnswerGrid(onlyPuzzle))
+	// abe, _ := getCompletePuzzle("gxd\\aarp\\1998\\mm1998-05-06.xd")
+	// bjørn := getPuzzle(abe)
+	// bi := createCompletedPuzzle(bjørn)
+	// fmt.Println(bi)
+
+	workingPuzzle, completedPuzzle := (initializeGame("gxd\\aarp\\1998\\mm1998-05-06.xd"))
+	fmt.Println(workingPuzzle)
+	fmt.Println("seperator")
+	fmt.Println(completedPuzzle)
 }
