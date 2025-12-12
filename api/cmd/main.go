b85384fbba681101d0db929b3c67ef2111eabf80
AbrAlexis <alexalex2620@gmail.com>
2025-12-12 20:28:34 +0100

    added docker

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,26 +1,35 @@
-package main
+package cmd
 
 import (
-	"fmt"
-	"lavkrydsord/backend/jsonutils"
 	"log"
 	"net/http"
+
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("../frontend")))
+    r := chi.NewRouter()
 
-	// http.HandleFunc("/puzzle", func(w http.ResponseWriter, r *http.Request) {
-	// 	// crosswordPuzzleStruct := crosswordPuzzle.InitializeGame("C:\\Users\\Default User.DESKTOP-F6CKQMA\\OneDrive\\Skrivebord\\lavkrydsord\\gxd\\aarp\\1998\\mm1998-07-08.xd")
-	// 	crosswordPuzzleStruct := crosswordPuzzle.InitializeGame("/home/abralexis/lavkrydsord/gxd/nytimes/2024/nyt2024-01-01.xd")
 
-	// 	jsonutils.MarshallPuzzleStruct(w, r, crosswordPuzzleStruct)
-	// })
+    r.Use(middleware.Logger)   // logs HTTP requests
+    r.Use(middleware.Recoverer) // recovers from panics
 
-	// http.HandleFunc("/puzzle/test1", jsonutils.HandleCheckPuzzle)
-	http.HandleFunc("/games", func(w http.ResponseWriter, r *http.Request) {
-		jsonutils.HandleFrontpage(w, r)
-	})
-	fmt.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	r.Handle("/*", http.FileServer(http.Dir("./static"))) // if you serve static files
+
+ 
+    // r.Get("/games", jsonutils.HandleFrontpage)
+
+    // 5️⃣ Example of more complex route
+    // r.Route("/puzzle", func(r chi.Router) {
+    //     r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+    //         // handle /puzzle
+    //     })
+    //     r.Get("/test1", jsonutils.HandleCheckPuzzle)
+    // })
+
+    // 6️⃣ Start the server
+    log.Println("Server running at http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
